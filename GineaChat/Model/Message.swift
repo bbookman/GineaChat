9910b67c1391b5e65f33eadedd1b70e947013820
Unknown <bruce.bookman@gmail.com>
2018-08-01 12:49:57 -0700

    New user and message model
    complete

---
+++ GineaChat/Model/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  GineaChat
+//
+//  Created by Bruce Bookman on 7/31/18.
+//  Copyright © 2018 Bruce Bookman. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    var messageText: String
+    var senderEmail: String
+}
